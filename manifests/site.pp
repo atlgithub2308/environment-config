f3725ae8677a7937befad272be6e7ee8a049c640
Teck Leong Ang <teckleong.ang@-C02F50USMD6V.local>
2021-11-12 11:05:43 +0800

    edited site.pp to change node names

--- manifests/site.pp
+++ manifests/site.pp
@@ -24,55 +24,56 @@ File { backup => false }
 # Puppet Enterprise console and External Node Classifiers (ENC's).
 #
 # For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
+
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
   # class { 'my_class': }
   # include java::install
   # include java
-  #notify { " Node ${fqdn} has no node definition": }
-  
+  # notify { " Node ${fqdn} has no node definition": }
+
   user { ['sysad1']:
     ensure => present
   }
 }
 
-node 'pemaster.atldemo.com' {
+node 'primary.atldemo.net' {
 
   user { ['sysad1']:
     ensure => present,
   }
 
-  ini_setting { 'policy-based autosigning':
-    setting => 'autosign',
-    path    => '/etc/puppetlabs/puppet/puppet.conf',
-    section => 'master',
-    value   => '/opt/puppetlabs/puppet/bin/autosign-validator',
-    notify  => Service['pe-puppetserver'],
-  }
+#  ini_setting { 'policy-based autosigning':
+#    setting => 'autosign',
+#    path    => '/etc/puppetlabs/puppet/puppet.conf',
+#    section => 'master',
+#    value   => '/opt/puppetlabs/puppet/bin/autosign-validator',
+#    notify  => Service['pe-puppetserver'],
+#  }
 
-  class { ::autosign:
-    ensure => 'latest',
-    config => {
-      'general' => {
-        'loglevel' => 'INFO',
-      },
-      'jwt_token' => {
-        'secret'   => 'hunter2',
-        'validity' => '7200',
-      }
-    },
-  }
+#  class { ::autosign:
+#    ensure => 'latest',
+#    config => {
+#      'general' => {
+#        'loglevel' => 'INFO',
+#      },
+#      'jwt_token' => {
+#        'secret'   => 'hunter2',
+#        'validity' => '7200',
+#      }
+#    },
+#  }
 }
 
-node 'node1redhat.atldemo.com' {
+node 'redhat1.atldemo.net' {
   lookup('role').include
 }
 
-node 'node3debian.atldemo.com' {
+node 'debian1.atldemo.net' {
   lookup('role').include
 }
 
-node 'node2windows.atldemo.com' {
+node 'windows1.atldemo.net' {
   include winservice
 }
